72492ad55baf24feba3ad6f52be910a684822b08
Aurpheus <62109447+Aurpheus@users.noreply.github.com>
2023-11-01 22:59:15 +0100

    pause menu is gud >:3

--- Game/scripts/loader.gd
+++ Game/scripts/loader.gd
@@ -13,15 +13,15 @@ func load_default_map():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("pause"):
-		get_hero().get_node("Camera2D").enabled = !get_hero().get_node("Camera2D").enabled
-		$menuCam.enabled = !$menuCam.enabled
-		$menus/pause.visible = !$menus/pause.visible
-		get_tree().paused = !get_tree().paused
+	if Input.is_action_just_pressed("pause") and $Level.get_child_count() > 0:
+		pause()
 	pass
 
 func pause():
-	print("pause !")
+	get_hero().get_node("Camera2D").enabled = !get_hero().get_node("Camera2D").enabled
+	$menuCam.enabled = !$menuCam.enabled
+	$menus/pause.visible = !$menus/pause.visible
+	get_tree().paused = !get_tree().paused
 	
 func get_hero():	
 	return $Level/Level/Hero
